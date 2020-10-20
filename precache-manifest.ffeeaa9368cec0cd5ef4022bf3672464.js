124ba3f37e74318cf5942fb847bb4f7f788e5016
Timokhin Pavel <60778218+Lesmor2001@users.noreply.github.com>
2020-10-20 20:28:43 +0600

    Add files via upload

---
+++ precache-manifest.ffeeaa9368cec0cd5ef4022bf3672464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6387db04944af242026a635eda99b6af",
+    "url": "/testgip/index.html"
+  },
+  {
+    "revision": "50be3a09ee30321afdad",
+    "url": "/testgip/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "baf35c04d79eea9bed35",
+    "url": "/testgip/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "50be3a09ee30321afdad",
+    "url": "/testgip/static/js/2.c0a3dd3b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/testgip/static/js/2.c0a3dd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf35c04d79eea9bed35",
+    "url": "/testgip/static/js/main.61e848d5.chunk.js"
+  },
+  {
+    "revision": "9e0b71a05206bf9239b0",
+    "url": "/testgip/static/js/runtime-main.d09bb371.js"
+  }
+]);
